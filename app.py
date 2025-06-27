760082708ffebf9efe17b7b3b575853bff84030b
Letícia Tambelli <leticiatambelli2@gmail.com>
2025-06-27 12:27:50 -0300

    Exercícios 2 e 3

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 class ContaBancaria:
     def __init__(self, titular='', saldo=0, ativo=False):
-        self.titular = titular
-        self.saldo = saldo
-        self.ativo = ativo
+        self._titular = titular.capitalize()
+        self._saldo = saldo
+        self._ativo = ativo
+
+    def __str__(self):
+        return f'Titular: {self._titular}, Saldo: {self._saldo}, Ativo: {self._ativo}'
+    
+    def ativar_conta(self):
+        self._ativo = True
+    
+conta1 = ContaBancaria('pâmela', 500)
+conta1.ativar_conta()
+conta2 = ContaBancaria('Rogério')
+
+print(conta1)
+print(conta2)
